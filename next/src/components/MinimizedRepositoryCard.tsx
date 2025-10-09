a1405287a42e3cf7a8aa0c856ae2b5f7e536781d
RaihanFuadSyakir <142875762+RaihanFuadSyakir@users.noreply.github.com>
2025-10-09 17:52:53 +0700

    add info about created and updated at on card repo

--- next/src/components/MinimizedRepositoryCard.tsx
+++ next/src/components/MinimizedRepositoryCard.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge"
-import { cn } from "@/lib/utils"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 import {
   Dialog,
   DialogContent,
@@ -10,92 +10,112 @@ import {
   DialogOverlay,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import RepositoryDetail from "./RepositoryDetail"
+} from "@/components/ui/dialog";
+import RepositoryDetail from "./RepositoryDetail";
 interface RepoCardProps {
-  repo: Repository
+  repo: Repository;
 }
 
-export function RepoCard({ repo}: RepoCardProps) {
+export function RepoCard({ repo }: RepoCardProps) {
   return (
     <>
-	<Dialog>
-	  <DialogTrigger>
-	   <Card
-	      className={cn(
-		"group relative w-full cursor-pointer transition-colors",
-		"hover:bg-muted/70"
-	      )}
-	    >
-	      {/* Hover overlay */}
-	      <div
-		className={cn(
-		  "absolute inset-0 flex items-center justify-center",
-		  "bg-black/40 text-white text-sm font-medium opacity-0 transition-opacity",
-		  "group-hover:opacity-100"
-		)}
-	      >
-		Click for detail
-	      </div>
+      <Dialog>
+        <DialogTrigger>
+          <Card
+            className={cn(
+              "group relative w-full cursor-pointer transition-colors",
+              "hover:bg-muted/70",
+            )}
+          >
+            {/* Hover overlay */}
+            <div
+              className={cn(
+                "absolute inset-0 flex items-center justify-center",
+                "bg-black/40 text-white text-sm font-medium opacity-0 transition-opacity",
+                "group-hover:opacity-100",
+              )}
+            >
+              Click for detail
+            </div>
 
-	      <CardHeader>
-		<CardTitle>{repo.name}</CardTitle>
-	      </CardHeader>
-	      <CardContent className="grid grid-cols-8 gap-2">
-		<div className="col-span-2">
-			<p className="text-sm flex justify-between font-semibold">
-			<span>Topics</span>
-			<span>:</span>
-			</p>{" "}
-		</div>
-		<div className="col-span-6 flex flex-wrap">
-		{repo.topics.length > 0 ? (
-		  repo.topics.map((topic) => (
-		    <Badge
-		    key={topic}
-		    variant="outline"
-		    className="bg-blue-100 text-blue-700 dark:bg-sky-900 dark:text-sky-300 m-1"
-		    >
-		      {topic}
-		    </Badge>
-		  ))
-		) : (
-		  "-"
-		)}
-		</div>
-		<div className="col-span-2">
-			<p className="text-sm flex justify-between font-semibold">
-			<span>Languages</span>
-			<span>:</span>
-			</p>{" "}
-		</div>
-		<div className="col-span-6 flex flex-wrap">
-		{repo.languages?.length || 0 > 0 ? (
-		  repo.languages?.map((lang) => (
-		    <Badge
-		    key={lang.name}
-		    variant="outline"
-		    className="bg-red-100 text-red-700 dark:bg-rose-900 dark:text-rose-300 m-1"
-		    >
-		      {lang.name}
-		    </Badge>
-		  ))
-		) : (
-		  "-"
-		)}
-		</div>
-	      </CardContent>
-	    </Card>
-	  </DialogTrigger>
-  	    <DialogOverlay>
-		  <DialogContent className="overflow-y-auto max-h-[90vh]">
-		    <DialogHeader>
-		      <DialogTitle>Repository Detail</DialogTitle>
-		    </DialogHeader>
-		    <RepositoryDetail repo={repo}/>
-		  </DialogContent>
-	    </DialogOverlay>
-	</Dialog>
+            <CardHeader>
+              <CardTitle>{repo.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="grid grid-cols-8 gap-2">
+              <div className="col-span-2">
+                <p className="text-sm flex justify-between font-semibold">
+                  <span>Topics</span>
+                  <span>:</span>
+                </p>{" "}
+              </div>
+              <div className="col-span-6 flex flex-wrap">
+                {repo.topics.length > 0
+                  ? repo.topics.map((topic) => (
+                      <Badge
+                        key={topic}
+                        variant="outline"
+                        className="bg-blue-100 text-blue-700 dark:bg-sky-900 dark:text-sky-300 m-1"
+                      >
+                        {topic}
+                      </Badge>
+                    ))
+                  : "-"}
+              </div>
+              <div className="col-span-2">
+                <p className="text-sm flex justify-between font-semibold">
+                  <span>Languages</span>
+                  <span>:</span>
+                </p>{" "}
+              </div>
+              <div className="col-span-6 flex flex-wrap">
+                {repo.languages?.length || 0 > 0
+                  ? repo.languages?.map((lang) => (
+                      <Badge
+                        key={lang.name}
+                        variant="outline"
+                        className="bg-red-100 text-red-700 dark:bg-rose-900 dark:text-rose-300 m-1"
+                      >
+                        {lang.name}
+                      </Badge>
+                    ))
+                  : "-"}
+              </div>
+              <div className="col-span-2">
+                <p className="text-sm flex justify-between font-semibold">
+                  <span>Created at</span>
+                  <span>:</span>
+                </p>{" "}
+              </div>
+              <div className="col-span-6 text-left">
+                {new Date(repo.created_at)
+                  .toISOString()
+                  .slice(0, 19)
+                  .replace("T", " ")}
+              </div>
+              <div className="col-span-2">
+                <p className="text-sm flex justify-between font-semibold">
+                  <span>Updated at</span>
+                  <span>:</span>
+                </p>{" "}
+              </div>
+              <div className="col-span-6 text-left">
+                {new Date(repo.updated_at)
+                  .toISOString()
+                  .slice(0, 19)
+                  .replace("T", " ")}
+              </div>
+            </CardContent>
+          </Card>
+        </DialogTrigger>
+        <DialogOverlay>
+          <DialogContent className="overflow-y-auto max-h-[90vh]">
+            <DialogHeader>
+              <DialogTitle>Repository Detail</DialogTitle>
+            </DialogHeader>
+            <RepositoryDetail repo={repo} />
+          </DialogContent>
+        </DialogOverlay>
+      </Dialog>
     </>
-  )
+  );
 }
